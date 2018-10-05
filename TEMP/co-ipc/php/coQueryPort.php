4ac34d10cbea8c7a9abb93a0d1d8a272dd93dd17
ninh <bhdninh@gmail.com>
2018-10-05 16:46:03 -0500

    added mapPort(), unmapPort(), getMap(), getSms()

--- TEMP/co-ipc/php/coQueryPort.php
+++ TEMP/co-ipc/php/coQueryPort.php
@@ -1,27 +1,41 @@
 <?php /* coQueryPort.php date:2018-10-05*/
   
-    ///Hello chu Ninh
-    $act = $_POST['act'];
-    $port_id = $_POST['port_id'];
-    $pnum = $_POST['pnum'];
-    $ptyp = $_POST['ptyp'];
-    $psta = $_POST['psta'];
-	$node = $_POST['node'];
-	$slot = $_POST['slot'];
-	$fac = $_POST['fac'];
-	$ftyp = $_POST['ftyp'];
+    ///Expected inputs
+    $act = "map"; //$_POST['act'];
+    $port_id = 1; 	//$_POST['port_id'];
+    $pnum = 1; 		//$_POST['pnum'];
+    $ptyp = ""; 	//$_POST['ptyp'];
+    $psta = "SF";	//$_POST['psta'];
+	$node = 1;	//$_POST['node'];
+	$slot = 1;		//$_POST['slot'];
+	$fac_id = 2;	//$_POST['fac_id'];
+	$fac = "";		//$_POST['fac'];
+	$ftyp = "CP";	//$_POST['ftyp'];
+	
+	//$db = mysqli_connect("localhost", "root", "Qaz!2345", "co5k");
+	$db = mysqli_connect("localhost", "ninh", "c0nsulta", "co5k");
+	if (mysqli_connect_errno())
+	{
+		$result["rslt"] = "fail";
+		$result["reason"] = mysqli_connect_error();
+		mysqli_close($db);
+		echo json_encode($result);
+		return;
+	}
 	
 	if ($act == "query") {
-		$result = queryPort();
+		$result = queryPort($node, $slot, $ptyp, $pnum, $psta);
 		echo json_encode($result);
 		return;
 	}
 	if ($act == "map") {
-		mapPort();
+		$result = mapPort($port_id, $fac_id);
+		echo json_encode($result);
 		return;
 	}
 	if ($act == "unmap") {
-		unmapPort();
+		$result = unmapPort($port_id, $fac_id);
+		echo json_encode($result);
 		return;
 	}
 	else {
@@ -32,70 +46,150 @@
 	}
 
 
-	function mapPort() {
-		global $port_id, $fac_id;
+	function mapPort($port_id, $fac_id) {
+		global $db;
 		
-		$qry = "SELECT * from t_ports, t_facs WHERE t_port.id=" . $port_id . " AND t_facs.id=" . $fac_id;
-        $res = $db_ct100->query($qry);
+		$result =[];
+		$map = getMap($port_id, $fac_id, "map");
+		if ($map["rslt"] == "fail") {
+			$result = $map;
+			mysqli_close($db);
+			return;
+		}
+		
+		$node = $map["rows"][0]["node"];
+		$slot = $map["rows"][0]["slot"];
+		$pnum = $map["rows"][0]["pnum"];
+		$ptyp = $map["rows"][0]["ptyp"];
+		$port = $node . "-" . $slot . "-" . $ptyp . "-" . $pnum;
+		$psta = $map["rows"][0]["psta"];
+		$ssta = $map["rows"][0]["ssta"];
+		$evt = "PT_MAP";
+		$sms = getSms($psta, $ssta, $evt);
+		if ($sms["rslt"] == "fail") {
+			$result = $sms;
+			mysqli_close($db);
+			return;
+		}
+			
+		$npsta = $sms["rows"][0]["npsta"];
+		$nssta = $sms["rows"][0]["nssta"];
+		$qry = "UPDATE t_ports SET fac_id=" . $fac_id . ", psta='" . $npsta . "', ssta='" . $nssta . "' WHERE id=" . $port_id;
+		$res = $db->query($qry);
+		$qry = "UPDATE t_facs SET port_id=" . $port_id . ", port='" . $port . "' WHERE id=" . $fac_id;
+		$res = $db->query($qry);
+				
+		return queryPort($node, $slot, $ptyp, $pnum, "");
+	}
+
+	function unmapPort($port_id, $fac_id) {
+		global $db;
+		
+		$result =[];
+		$map = getMap($port_id, $fac_id, "unmap");
+		if ($map["rslt"] == "fail") {
+			mysqli_close($db);
+			return $map;
+		}
+		echo "here 1\n";
+		$node = $map["rows"][0]["node"];
+		$slot = $map["rows"][0]["slot"];
+		$pnum = $map["rows"][0]["pnum"];
+		$ptyp = $map["rows"][0]["ptyp"];
+		$port = $node . "-" . $slot . "-" . $ptyp . "-" . $pnum;
+		$psta = $map["rows"][0]["psta"];
+		$ssta = $map["rows"][0]["ssta"];
+		$evt = "PT_UNMAP";
+		echo "here 2\n";
+		$sms = getSms($psta, $ssta, $evt);
+		echo "here 3\n";
+		if ($sms["rslt"] == "fail") {
+			mysqli_close($db);
+			return $sms;
+		}
+		$npsta = $sms["rows"][0]["npsta"];
+		$nssta = $sms["rows"][0]["nssta"];
+		$qry = "UPDATE t_ports SET fac_id=" . 0 . ", psta='" . $npsta . "', ssta='" . $nssta . "' WHERE id=" . $port_id;
+		$res = $db->query($qry);
+		$qry = "UPDATE t_facs SET port_id=" . 0 . ", port='' WHERE id=" . $fac_id;
+		$res = $db->query($qry);
+				
+		return queryPort($node, $slot, "", "", "");
+	}
+	
+	function getSms($psta, $ssta, $evt) {
+		global $db;
+
+		$qry = "SELECT * FROM t_sms WHERE evt='" . $evt . "' AND psta='" . $psta . "' AND ssta='" . $ssta . "'";
+        $res = $db->query($qry);
         if (!$res) {
             $result["rslt"] = "fail";
-            $result["reason"] = mysqli_error($db_ct100);
-=======
-<?php /* coQueryPort.php */
-    ///Hello chu Ninh
-    $port = $_POST['port'];
-
-    //$db_ct100 = mysqli_connect("localhost", "root", "Qaz!2345", "co5k");
-    $db_ct100 = mysqli_connect("localhost", "ninh", "c0nsulta", "co5k");
-    if (mysqli_connect_errno())
-    {
-        $result["rslt"] = "fail";
-        $result["reason"] = mysqli_connect_error();
-    }
-
-    else {
-        if ($port != "all") {
-            $result["rslt"] = "failed";
-            $result["reason"] = "is under development";
->>>>>>> 90731cf1c32e5f893e4ddb7083ab3dcca8ceacb9
+            $result["reason"] = mysqli_error($db);
         }
         else {
             $rows = [];
-            $result["rslt"] = "success";
 			if ($res->num_rows > 0) {
                 while ($row = $res->fetch_assoc()) {
                     $rows[] = $row;
                 }
+				$result["rslt"] = "success";
+				$result["rows"] = $rows;
             }
-            $result["rows"] = $rows;
+            else {
+				$result["rslt"] = "fail";
+				$result["reason"] = "Invalid SMS";
+			}
 		}
-		mysqli_close($db_ct100);
 		return $result;
 	}
 	
-	
-	
-	function queryPort() {
-		global $port_id, $pnum, $ptyp, $node, $slot, $psta;
 		
-		//$db_ct100 = mysqli_connect("localhost", "root", "Qaz!2345", "co5k");
-		$db_ct100 = mysqli_connect("localhost", "ninh", "c0nsulta", "co5k");
-		if (mysqli_connect_errno())
-		{
-			$result["rslt"] = "fail";
-			$result["reason"] = mysqli_connect_error();
-			return $result;
+	function getMap($port_id, $fac_id, $typ) {
+		global $db;
+		
+		if ($typ == "map")
+			$qry = "SELECT * from t_ports, t_facs WHERE t_ports.id=" . $port_id . " AND t_facs.id=" . $fac_id . " AND t_facs.port =''";
+ 		else
+			$qry = "SELECT * from t_ports, t_facs WHERE t_ports.id=" . $port_id . " AND t_facs.id=" . $fac_id . " AND t_facs.port <>''";
+
+		$res = $db->query($qry);
+        if (!$res) {
+            $result["rslt"] = "fail";
+            $result["reason"] = mysqli_error($db_ct100);
+        }
+        else {
+            $rows = [];
+            if ($res->num_rows > 0) {
+                while ($row = $res->fetch_assoc()) {
+                    $rows[] = $row;
+                }
+				$result["rslt"] = "success";
+	            $result["rows"] = $rows;
+			}
+            else {
+				$result["rslt"] = "fail";
+				$result["reason"] = "Invalid FAC";
+			}
 		}
+		return $result;
+	}
+		
+	
+	
+	function queryPort($node, $slot, $ptyp, $pnum, $psta) {
+		global $db;
+		
+		echo $node . "-" . $slot . "-" . $ptyp . "-" . $pnum . ", " . $psta . "\n";
 		
 		$qry = "SELECT t_ports.node, t_ports.slot, t_ports.pnum, t_ports.ptyp, t_ports.psta, t_facs.fac, t_ckts.ckid ";
 		$qry .= "FROM t_ports LEFT JOIN t_facs ON t_ports.fac_id = t_facs.id LEFT JOIN t_ckts ON t_ports.ckt_id = t_ckts.id";
 		$qry .= " WHERE t_ports.pnum LIKE '%$pnum%%' AND t_ports.ptyp LIKE '%$ptyp%%' AND t_ports.node=" . $node;
 		$qry .= " AND t_ports.slot LIKE '%$slot%%' AND t_ports.psta LIKE '%$psta%%'";
         
-        $res = $db_ct100->query($qry);
+        $res = $db->query($qry);
         if (!$res) {
             $result["rslt"] = "fail";
-            $result["reason"] = mysqli_error($db_ct100);
+            $result["reason"] = mysqli_error($db);
         }
         else {
             $rows = [];
@@ -107,7 +201,6 @@
             }
             $result["rows"] = $rows;
 		}
-		mysqli_close($db_ct100);
 		return $result;
 	}
 
