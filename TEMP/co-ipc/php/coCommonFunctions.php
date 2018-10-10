718c0ef2637e6b136a531cfc4bde169672425735
ninh <bhdninh@gmail.com>
2018-10-10 16:28:22 -0500

    Added Common Functions

---
+++ TEMP/co-ipc/php/coCommonFunctions.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Copy Right @ 2018
+ * BHD Solutions, LLC.
+ * Project: CO-IPC
+ * Filename: coCommonFunctions.php
+ * Change history: 
+ * 2018-10-10: created (Ninh)
+ */
+ 
+
+	function userPermission($fnc, $user) {
+		//temporary permit all users
+		return true;
+	}
+  
+?>
