75718bbb08e16c83368d40a12f9727622542a089
ninh <bhdninh@gmail.com>
2018-10-10 16:03:59 -0500

    added ackAlm()

--- TEMP/co-ipc/php/coQueryAlm.php
+++ TEMP/co-ipc/php/coQueryAlm.php
@@ -1,130 +1,161 @@
-<?php /* coQueryAlm.php */
-    $act = $_POST['act'];
-    //$act="query";
+<?php
+/*
+ * Copy Right @ 2018
+ * BHD Solutions, LLC.
+ * Project: CO-IPC
+ * Filename: coQueryAlm.php
+ * Change history: 
+ * 09-27-2018: created (Tracy)
+ */
 
-	if ($act == "query") {
-		queryAlm();
-		return;
-	}
-	if ($act == "add") {
-    // addAlm() is not being used here
-	// 	addAlm();
-		return;
-	}
-	else {
- 		$result["rslt"] = "fail";
-		$result["reason"] = "ACTION " . $act . " is under development or not supported";
+	include "coCommonFunctions.php";
+ 
+    $act = $_POST['act'];
+    $ack = $_POST['ack'];
+    $almid = $_POST['almid'];
+    $descr = $_POST['descr'];
+    $user = $_POST['user'];
+    
+    //test data
+    //$act="ack";
+	//$ack="NINH";
+	//$almid = 1;
+	//$descr = "ABC";
+	
+	//$db = mysqli_connect("localhost", "root", "Qaz!2345", "co5k");
+	$db = mysqli_connect("localhost", "ninh", "c0nsulta", "co5k");
+	if (mysqli_connect_errno()) {
+		$result["rslt"] = "fail";
+		$result["reason"] = mysqli_connect_error();
+		mysqli_close($db);
 		echo json_encode($result);
 		return;
 	}
+	
+	if ($act == "query") {
+		$result = queryAlm();
+		echo json_encode($result);
+		return;
+	}
+	
+	if ($act == "ack") {
+		$result = ackAlm($ack);
+		echo json_encode($result);
+		return;
+	}
+
+
 	
 	function queryAlm() {
-		global $ack, $sa, $date, $sev, $cond;
-		$db_ct100 = mysqli_connect("localhost", "ninh", "c0nsulta", "co5k");
-		if (mysqli_connect_errno())
-		{
-			$result["rslt"] = "fail";
-			$result["reason"] = mysqli_connect_error();
-			echo json_encode($result);
-			return;
-		}
-
-        if ( $ack == '')
-                $qry = "select id, almid, ack,sa, date, time, src, type, cond, sev, psta, ssta,descr  FROM t_alms  where sa like '%$sa%%' and date like '%$date%%' and sev like '%$sev%%' and cond like '%$cond%%' "; 
-        else if ( $sa == '') 
-                $qry = "select id, almid, ack,sa, date, time, src, type, cond, sev, psta, ssta,descr  FROM t_alms  where ack like '%$ack%%' and date like '%$date%%' and sev like '%$sev%%' and cond like '%$cond%%' "; 
-        else if ( $date == '')
-                $qry = "select id, almid, ack,sa, date, time, src, type, cond, sev, psta, ssta,descr  FROM t_alms  where ack like '%$ack%%' and sa like '%$sa%%' and sev like '%$sev%%' and cond like '%$cond%%' "; 
-        else if ( $sev == '')
-                $qry = "select id, almid, ack,sa, date, time, src, type, cond, sev, psta, ssta,descr  FROM t_alms  where ack like '%$ack%%' and sa like '%$sa%%' and date like '%$date%%' and cond like '%$cond%%' "; 
-        else if ( $cond == '')
-                $qry = "select id, almid, ack,sa, date, time, src, type, cond, sev, psta, ssta,descr  FROM t_alms  where ack like '%$ack%%' and sa like '%$sa%%' and date like '%$date%%' and sev like '%$sev%%' "; 
-        else
-                $qry = "select id, almid, ack,sa, date, time, src, type, cond, sev, psta, ssta,descr  FROM t_alms  where ack like '%$ack%%' and sa like '%$sa%%' and date like '%$date%%' and sev like '%$sev%%' and cond like '%$cond%%' "; 
+		global $db, $user;
         
-               $res = $db_ct100->query($qry);
-               if (!$res) {
-                  $result["rslt"] = "fail";
-                  $result["reason"] = mysqli_error($db_ct100);
-               }
-               else {
-                  $rows = array();
-                  if ($res->num_rows == NULL) {
-                     $result["rslt"] = "fail";
-                     $result["reason"] = "There is no matching data!";
-                  }
-                  else
-                  {
-                     while ($row = $res->fetch_assoc()) {
-                       $rows[] = $row;
-                     }  
-                     $result["rslt"] = "success";
-                     $result["rows"] = $rows;
-                  }
-         
-              }
-   	      echo json_encode($result);
-	      mysqli_close($db_ct100);
-	}
-   
-	function addAlm() {
-		global $almid, $ack, $sa, $date, $time, $src, $type, $cond, $sev, $psta, $ssta, $descr;
-		//$db_ct100 = mysqli_connect("localhost", "root", "Qaz!2345", "co5k");
-		$db_ct100 = mysqli_connect("localhost", "ninh", "c0nsulta", "co5k");
-		if (mysqli_connect_errno())
-		{
+        if (userPermission("almadm",$user) == false) {
 			$result["rslt"] = "fail";
-			$result["reason"] = mysqli_connect_error();
-			echo json_encode($result);
-			return;
+			$result["reason"] = "Permission Denied";
+			return $result;
 		}
-
-        /*
-		$qry = "insert into t_alms values(0,'";
-		if ($type != "") {
-		  $qry .= $type  . "','";
-		  if ($sa != "") {
-		     $qry  .= $sa  . "','";
-		     if ($src  != "") {
-			$qry .= $src  . "','";
-			if ($sev  != "") {
-		           $qry .= $sev . "'";
-			   }
-			   else {
-			     $qry .= "'";
-			   }
-					$qry .= ",'',0)";
-					$res = $db_ct100->query($qry);
-					if (!$res) {
-						$result["rslt"] = "fail";
-						$result["reason"] = mysqli_error($db_ct100);
-					}
-					else {
-						$qry = "select * FROM t_facs";
-						$res = $db_ct100->query($qry);
-						if (!$res) {
-							$result["rslt"] = "fail";
-							$result["reason"] = mysqli_error($db_ct100);
-						}
-						else {
-							$rows = [];
-							if ($res->num_rows > 0) {
-								while ($row = $res->fetch_assoc()) {
-									$rows[] = $row;
-								}
-								$result["rslt"] = "success";
-								$result["rows"] = $rows;
-							}
-						}
-					}
-		       } else { $result["rslt"] = "fail"; $result["reason"] = "Invalid src :" . $src ; }
-		   } else { $result["rslt"] = "fail"; $result["reason"] = "Invalid sa :" . $sa; }
-		} else { $result["rslt"] = "fail"; $result["reason"] = "Invalid type :" . $type;
+		
+        $qry = "select *  FROM t_alms"; 
+        $res = $db->query($qry);
+        if (!$res) {
+			$result["rslt"] = "fail";
+			$result["reason"] = mysqli_error($db);
 		}
-        */
-
-		echo json_encode($result);
-		mysqli_close($db_ct100);
+		else {
+			$rows = [];
+			if ($res->num_rows > 0) {
+				while ($row = $res->fetch_assoc()) {
+                       $rows[] = $row;
+				}  
+				$result["rslt"] = "success";
+				$result["rows"] = $rows;
+			}
+        }
+		mysqli_close($db);
+		return $result;
 	}
-	
+
+
+	function ackAlm() {
+		global $db, $almid, $ack, $descr;
+		
+        //validate $ack: check user permission for acknowledge alarm
+        if (userPermission("almadm",$ack) == false) {
+			$result["rslt"] = "fail";
+			$result["reason"] = "Permission Denied";
+			return $result;
+		}
+
+		if ($descr == "") {
+			$result["rslt"] = "fail";
+			$result["reason"] = "Missing Remark";
+			return $result;
+		}
+			
+		$qry = "UPDATE t_alms SET ack='" . $ack . "', cond='ACK', descr='" . "' WHERE almid=" . $almid;
+		$res = $db->query($qry);
+		if (!$res) {
+			$result["rslt"] = "fail";
+			$result["reason"] = mysqli_error($db);
+		}
+		else {
+			$qry = "select *  FROM t_alms"; 
+			$res = $db->query($qry);
+			$rows = [];
+			if ($res->num_rows > 0) {
+				while ($row = $res->fetch_assoc()) {
+					$rows[] = $row;
+				}
+				$result["rslt"] = "success";
+				$result["rows"] = $rows;
+			}
+		}
+		mysqli_close($db);
+		return $result;
+	}
+		 
+
+	function unackAlm() {
+		global $db, $almid, $ack, $descr;
+		
+        //validate $ack: check user permission for acknowledge alarm
+        if (userPermission("almadm",$ack) == false) {
+			$result["rslt"] = "fail";
+			$result["reason"] = "Permission Denied";
+			return $result;
+		}
+		if ($descr == "") {
+			$result["rslt"] = "fail";
+			$result["reason"] = "Missing Remark";
+			return $result;
+		}
+			
+		$qry = "UPDATE t_alms SET ack='" . $ack . "', cond='ACK', descr='" . "' WHERE almid=" . $almid;
+		$res = $db->query($qry);
+		if (!$res) {
+			$result["rslt"] = "fail";
+			$result["reason"] = mysqli_error($db);
+		}
+		else {
+			$qry = "select *  FROM t_alms"; 
+			$res = $db->query($qry);
+			$rows = [];
+			if ($res->num_rows > 0) {
+				while ($row = $res->fetch_assoc()) {
+					$rows[] = $row;
+				}
+				$result["rslt"] = "success";
+				$result["rows"] = $rows;
+			}
+		}
+		mysqli_close($db);
+		return $result;
+	}
+		 
+/*
+	function userPermission($fnc, $user) {
+		//temporary permit all users
+		return true;
+	}
+*/
 ?>
