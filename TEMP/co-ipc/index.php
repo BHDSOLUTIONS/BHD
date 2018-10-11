1f9fbba329ffd351f754f066229e236633013b1c
thanhpv8 <thanhpv678@gmail.com>
2018-10-11 15:13:37 -0500

    update

--- TEMP/co-ipc/index.php
+++ TEMP/co-ipc/index.php
@@ -3,6 +3,7 @@
 include './html/co-ipc-menu.html';
 include './html/co-ipc-fac.html';
 include './html/co-ipc-portmap.html';
+include './html/co-ipc-prov.html';
 include './html/co-ipc-alarm.html';
 include './html/co-ipc-user.html';
 include './html/co-ipc-facModal.html';
