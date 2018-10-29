f22f366ce07d694c10d169d4a81b66c93d2167d2
Tracy Ta <tracy.ta@gmail.com>
2018-10-29 12:42:56 -0500

    Updating css file to accomodate overflow-y to auto

--- co-ipc/GUI/php/coCommonFunctions.php
+++ co-ipc/GUI/php/coCommonFunctions.php
@@ -19,9 +19,9 @@
 		
 		public function __construct() {
 			$this->host = "localhost";
-			//$this->dbname = "co5k"; $this->ui = "ninh"; $this->pw = "c0nsulta";
+			$this->dbname = "co5k"; $this->ui = "ninh"; $this->pw = "c0nsulta";
 			//$this->dbname = "co5k"; $this->ui = "root"; $this->pw = "Qaz!2345";
-			$this->dbname = "wizzis5_co5k"; $this->ui = "wizzis5_co5kuser"; $this->pw = "co5kuser1";
+			//$this->dbname = "wizzis5_co5k"; $this->ui = "wizzis5_co5kuser"; $this->pw = "co5kuser1";
 			
 			
 			$this->con = mysqli_connect($this->host, $this->ui, $this->pw, $this->dbname);
